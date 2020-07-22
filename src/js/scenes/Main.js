a38ce7c10388259cfef39cb74a95f7cf57435cc8
Edwin Joseph <edwinj3829@googlemail.com>
2020-07-22 22:16:47 +0100

    feat(platform tut): add local assets and render sky

--- src/js/scenes/Main.js
+++ src/js/scenes/Main.js
@@ -1,46 +1,26 @@
 import Phaser from 'phaser';
+import images from '../../assets/*.png'
 
 class Main extends Phaser.Scene {
   constructor() {
     super({
       key: 'main',
-      physics: {  
-        default: 'arcade',
-        arcade: {
-          gravity: { y: 200 }
-        },
-      }
     });
   }
 
   preload() {
-    this.load.setBaseURL('http://labs.phaser.io');
-
-    this.load.image('sky', 'assets/skies/space3.png');
-    this.load.image('logo', 'assets/sprites/phaser3-logo.png');
-    this.load.image('red', 'assets/particles/red.png');
+    this.load.image('sky', images.sky);
+    this.load.image('ground', images.platform);
+    this.load.image('star', images.star);
+    this.load.image('bomb', images.bomb);
+    this.load.spritesheet('player', images.dude, {
+      frameWidth: 32,
+      frameHeight: 48,
+    });
   }
 
   create() {
-    this.add.image(400, 300, 'sky');
-
-    const particles = this.add.particles('red');
-    const emitter = particles.createEmitter({
-      speed: 100,
-      scale: {
-        start: 1,
-        end: 0
-      },
-      blendMode: 'ADD',
-    });
-
-    const logo = this.physics.add.image(400, 100, 'logo');
-
-    logo.setVelocity(100, 200);
-    logo.setBounce(1, 1);
-    logo.setCollideWorldBounds(true);
-
-    emitter.startFollow(logo);
+    this.add.image(0, 0, 'sky').setOrigin(0, 0);
   }
 }
 
