1c30b1d40e6c0655978f5976dc058b54dff8cfa1
Edwin Joseph <edwinj3829@googlemail.com>
2020-07-22 23:38:35 +0100

    feat(platform tut): add player sprite with animations

--- src/js/scenes/Main.js
+++ src/js/scenes/Main.js
@@ -2,6 +2,7 @@ import Phaser from 'phaser';
 import images from '../../assets/*.png'
 
 let platforms;
+let player;
 
 class Main extends Phaser.Scene {
   constructor() {
@@ -41,6 +42,29 @@ class Main extends Phaser.Scene {
     platforms.create(600, 400, 'ground');
     platforms.create(50, 250, 'ground');
     platforms.create(750, 220, 'ground');
+
+    player = this.physics.add.sprite(100, 450, 'player');
+
+    player.setBounce(0.2);
+    player.setCollideWorldBounds();
+
+    this.anims.create({
+      key: 'left',
+      frames: this.anims.generateFrameNumbers('player', { start: 0, end: 3 }),
+      frameRate: 10,
+      repeat: -1,
+    })
+    this.anims.create({
+      key: 'turn',
+      frames: [{ key: 'player', frame: 4 }],
+      frameRate: 20
+    });
+    this.anims.create({
+      key: 'right',
+      frames: this.anims.generateFrameNumbers('player', { start: 5, end: 8 }),
+      frameRate: 10,
+      repeat: -1
+    });
   }
 }
 
